b0520ff1a833e8fdbbf4540a06a304cc2c88e0fe
thiendo261 <thiendo3831@gmail.com>
2020-10-03 10:06:09 +0700

    Fix layouts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import Input from './components/Input/Input';
-import ListItem from './components/ListItem/ListItem';
-import Options from './components/Options/Options';
+import "./App.css";
+import Input from "./components/Input/Input";
+import ListItem from "./components/ListItem/ListItem";
+import Options from "./components/Options/Options";
 
 function App() {
-  !localStorage.getItem('todoList') && localStorage.setItem('todoList', '[]');
+  !localStorage.getItem("todoList") && localStorage.setItem("todoList", "[]");
 
-  const [data, setData] = useState(JSON.parse(localStorage.getItem('todoList')));
+  const [data, setData] = useState(
+    JSON.parse(localStorage.getItem("todoList"))
+  );
 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <Input data={data} setData={setData} />
-      {
-        !data.length
-          ? <h3>Nothing here!</h3>
-          : data.map((item, index) => <ListItem
-              key={index}
-              item={item}
-              index={index}
-              data={data}
-              setData={setData}
-            />)
-      }
+      {!data.length ? (
+        <h3>Nothing here!</h3>
+      ) : (
+        data.map((item, index) => (
+          <ListItem
+            key={index}
+            item={item}
+            index={index}
+            data={data}
+            setData={setData}
+          />
+        ))
+      )}
       <Options data={data} setData={setData} />
     </div>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import './Options.css';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Options.css";
 
-const Options = props => {
+const Options = (props) => {
   const { data, setData } = props;
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCount(data.filter(item => item.completed === false).length);
+    setCount(data.filter((item) => item.completed === false).length);
   }, [data]);
 
   const deleteCompleted = () => {
-    const currentData = JSON.parse(localStorage.getItem('todoList'));
-    const newData = currentData.filter(item => item.completed === false);
+    const currentData = JSON.parse(localStorage.getItem("todoList"));
+    const newData = currentData.filter((item) => item.completed === false);
     setData(newData);
-    localStorage.setItem('todoList', JSON.stringify(newData));
+    localStorage.setItem("todoList", JSON.stringify(newData));
   };
 
   return (
     <div className="Options">
       <div className="CountItem">
-        {
-          count === 1 ? `${count} task left` : count === 0
-            ? 'Have a nice day!' : `${count} tasks left`
-        }
+        {count === 1
+          ? `${count} task left`
+          : count === 0
+          ? "Have a nice day!"
+          : `${count} tasks left`}
       </div>
-      {
-        data.find(item => item.completed === true) &&
-        <button className="Btn" onClick={deleteCompleted}>Delete Completed</button>
-      }
+      {data.find((item) => item.completed === true) && (
+        <button className="Btn" onClick={deleteCompleted}>
+          Delete Completed
+        </button>
+      )}
     </div>
   );
 };
 
 Options.propsTypes = {
   data: PropTypes.array.isRequired,
-  setData: PropTypes.func.isRequired
-}
+  setData: PropTypes.func.isRequired,
+};
 
 export default Options;
